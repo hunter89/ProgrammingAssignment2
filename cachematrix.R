d506c5f1407699010f7ff08ea774e735148249a0
shubham pawar <stg.shubham@gmail.com>
2014-12-22 03:07:13 +0530

    sample commit to test git GUI

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,16 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  inverse <- NULL
+  set <- function(y){
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(i) inverse <<- i
+  getinverse <- function() inverse
+  
+  list <- 
 
 }
 
